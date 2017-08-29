04f7e3d2d85e5d0fb74f41c453754f9d0c83dee2
Bay <bay1ts@Gmail.com>
2017-08-29 17:16:06 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+ 
+HOSTNAME=`hostname`
+
+cd "$ZOO_CONF_DIR"
+Index="$IFS" IFS="-" arr=($HOSTNAME) IFS="$OLD_IFS" echo  ${arr[@]:1:1}
+HOSTN="$IFS" IFS="-" arr=($HOSTNAME) IFS="$OLD_IFS" echo  ${arr[@]:0:1}
+
+ 
+((MYID=$Index+1)) 
+ZK="$HOSTN"+00
+#IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$ZK}' | cut -d / -f 1`
+
+if [ "$Index"=="00" ]
+then
+  echo "server.$MYID=$HOSTNAME:2888:3888;2181" >> "$ZOO_CONF_DIR"/zoo.cfg.dynamic
+  zkServer-initialize.sh --force --myid=$MYID
+  zkServer.sh start-foreground
+  
+else
+  echo "`zkCli.sh -server $ZK:2181 get /zookeeper/config|grep ^server`" >> "$ZOO_CONF_DIR"/zoo.cfg.dynamic
+  echo "server.$MYID=$HOSTNAME:2888:3888:observer;2181" >> "$ZOO_CONF_DIR"/zoo.cfg.dynamic
+  cp "$ZOO_CONF_DIR"/zoo.cfg.dynamic "$ZOO_CONF_DIR"/zoo.cfg.dynamic.org
+  zkServer-initialize.sh --force --myid=$MYID
+  zkServer.sh start
+  zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$HOSTNAME:2888:3888:participant;2181"
+  zkServer.sh stop
+  zkServer.sh start-foreground
+fi
+
+/docker-entrypoint.sh/docker-entrypoint.sh
