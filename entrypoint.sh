fdfc08175d8ac8f826eef81f224fdbb69045bb52
Bay <bay1ts@Gmail.com>
2017-10-09 13:59:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,7 @@ MYID=`expr $Index + 1`
 ZK=${HOSTN}"-0"
 echo "first zk is ------"
 echo "$ZK"
+echo "$MYID"
 #IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$ZK}' | cut -d / -f 1`
 
 if [ "$Index"=="0" ]
