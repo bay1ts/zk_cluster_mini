46f17a7224e6c9d3dfa25b2dd150804c1bd63169
Bay <bay1ts@Gmail.com>
2017-10-19 01:45:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ else
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   echo "$MYID" >/tmp/zookeeper/myid
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
-  /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
+  /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$HOSTNAME:2888:3888:participant;2181"
   /tmp/zookeeper/bin/zkServer.sh stop
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
   
