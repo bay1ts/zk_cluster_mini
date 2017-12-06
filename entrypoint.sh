1a31a17e332a490893829a078c9e79cb5bb10359
Bay <bay1ts@Gmail.com>
2017-12-06 12:08:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
  
 HOSTNAME=`hostname`
 
-cd /tmp/zookeeper
+cd /tmp/zkapp
 OLD_IFS="$IFS" 
 IFS="-" 
 arr=($HOSTNAME) 
@@ -15,9 +15,9 @@ MYID=`expr $Index + 1`
 ZK=${HOSTN}"-0"
 #IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$ZK}' | cut -d / -f 1`
 
-sed -i '177i if [ -e "/java_mem_common.sh" ];then' /tmp/zookeeper/bin/zkServer.sh
-sed -i '178i . "/java_mem_common.sh"' /tmp/zookeeper/bin/zkServer.sh
-sed -i "179i fi" /tmp/zookeeper/bin/zkServer.sh
+sed -i '177i if [ -e "/java_mem_common.sh" ];then' /tmp/zkapp/bin/zkServer.sh
+sed -i '178i . "/java_mem_common.sh"' /tmp/zkapp/bin/zkServer.sh
+sed -i "179i fi" /tmp/zkapp/bin/zkServer.sh
 
 /peer-finder -on-start=/on-start.sh -service=$(echo $SERVICE_NAME)
 echo "begin check is cluster"
@@ -27,23 +27,23 @@ then
   node=cat /zk/zk-data/first_node
   echo "adding to existed node $node"
   echo "`bin/zkCli.sh -server $node:2181 get /zookeeper/config|grep ^server`" 
-  echo "server.1=$ZK.$HOSTN:2888:3888:participant;0.0.0.0:2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
-  echo "server.$MYID=$HOSTNAME.$HOSTN:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
-  cp /tmp/zookeeper/conf/zoo.cfg.dynamic /tmp/zookeeper/conf/zoo.cfg.dynamic.org
-  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
-  echo "$MYID" >/tmp/zookeeper/myid
-  echo "$MYID" >/tmp/zookeeper/zookeeper_server.pid
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
-  /tmp/zookeeper/bin/zkCli.sh -server $ZK.$HOSTN:2181 reconfig -add "server.$MYID=$HOSTNAME.$HOSTN:2888:3888:participant;2181"
-  /tmp/zookeeper/bin/zkServer.sh stop
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' exec /tmp/zookeeper/bin/zkServer.sh start-foreground
+  echo "server.1=$ZK.$HOSTN:2888:3888:participant;0.0.0.0:2181" >> /tmp/zkapp/conf/zoo.cfg.dynamic
+  echo "server.$MYID=$HOSTNAME.$HOSTN:2888:3888:observer;2181" >> /tmp/zkapp/conf/zoo.cfg.dynamic
+  cp /tmp/zkapp/conf/zoo.cfg.dynamic /tmp/zkapp/conf/zoo.cfg.dynamic.org
+  /tmp/zkapp/bin/zkServer-initialize.sh --force --myid=$MYID
+  echo "$MYID" >/tmp/zkapp/myid
+  echo "$MYID" >/tmp/zkapp/zookeeper_server.pid
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zkapp/bin/zkServer.sh start
+  /tmp/zkapp/bin/zkCli.sh -server $ZK.$HOSTN:2181 reconfig -add "server.$MYID=$HOSTNAME.$HOSTN:2888:3888:participant;2181"
+  /tmp/zkapp/bin/zkServer.sh stop
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' exec /tmp/zkapp/bin/zkServer.sh start-foreground
 else
   echo "first node"
   echo "server.$MYID=$HOSTNAME.$HOSTN:2888:3888;2181"
-  echo "server.$MYID=$HOSTNAME.$HOSTN:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
-  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid="$MYID"
-  echo "$MYID" >/tmp/zookeeper/myid
-  echo "$MYID" >/tmp/zookeeper/zookeeper_server.pid
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' exec /tmp/zookeeper/bin/zkServer.sh start-foreground
+  echo "server.$MYID=$HOSTNAME.$HOSTN:2888:3888;2181" >> /tmp/zkapp/conf/zoo.cfg.dynamic
+  /tmp/zkapp/bin/zkServer-initialize.sh --force --myid="$MYID"
+  echo "$MYID" >/tmp/zkapp/myid
+  echo "$MYID" >/tmp/zkapp/zookeeper_server.pid
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' exec /tmp/zkapp/bin/zkServer.sh start-foreground
   
 fi
